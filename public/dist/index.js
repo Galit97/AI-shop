e0cad901f42fac75c682a9bf026b88590ba3b51b
Toharkenin <109432113+Toharkenin@users.noreply.github.com>
2024-12-03 18:50:45 +0200

    .

--- public/dist/index.js
+++ public/dist/index.js
@@ -1,2 +1,8 @@
 var main = document.querySelector('#main');
 var header = document.querySelector('#header');
+if (main && header) {
+    main.appendChild(header);
+}
+else {
+    console.error('Either #main or #header element is not found in the DOM.');
+}
