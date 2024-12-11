ff898f2946cf98f6b0d230ec91117253c7a80ab4
Toharkenin <109432113+Toharkenin@users.noreply.github.com>
2024-12-11 11:54:52 +0200

    .

--- src/controllers/products/dist/getProduct.js
+++ src/controllers/products/dist/getProduct.js
@@ -44,11 +44,10 @@ exports.getProducts = function (req, res) { return __awaiter(void 0, void 0, voi
         switch (_a.label) {
             case 0:
                 _a.trys.push([0, 2, , 3]);
-                return [4 /*yield*/, productModel_1.ProductModel.find()
-                    // .populate('category');
-                ];
+                return [4 /*yield*/, productModel_1.ProductModel.find()];
             case 1:
                 products = _a.sent();
+                console.log("products", products);
                 // .populate('category');
                 res.status(200).json(products);
                 return [3 /*break*/, 3];
--- src/controllers/products/getProduct.ts
+++ src/controllers/products/getProduct.ts
@@ -4,6 +4,7 @@ import { ProductModel } from '../../models/productModel';
 export const getProducts = async (req: Request, res: Response) => {
   try {
     const products = await ProductModel.find()
+    console.log("products", products);
     // .populate('category');
     res.status(200).json(products);
   } catch (error) {
